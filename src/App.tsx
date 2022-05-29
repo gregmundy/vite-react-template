ba29f2a71233da7e9aaaec7ddf91593890558d3e
Greg Mundy <realgregmundy@gmail.com>
2022-05-29 18:19:58 -0400

    feat: add tailwind configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './App.less'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <p className="text-3xl font-bold underline">Tailwind Installed</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
